bbd367615423d101726510f5e30487b048513a4d
Adam Kelly <adam@cthulahoops.org>
2024-10-02 19:02:20 +0100

    Commit all the unsaved files.

---
+++ src/Note.tsx
@@ -0,0 +1,17 @@
+import ReactMarkdown from 'react-markdown';
+
+export default function({ content, timestamp }: { content: string, timestamp: number }) {
+  const date = new Date(timestamp);
+  return (
+    <article className="note">
+      <ReactMarkdown>{content}</ReactMarkdown>
+      {date &&
+        <footer>
+          <time dateTime={date.toISOString()}>
+            {date.toString()}
+          </time>
+        </footer>
+      }
+    </article>
+  );
+};
