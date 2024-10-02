8e693ddb50076c5342f6062458e7c7721afa4271
Adam Kelly <adam@cthulahoops.org>
2024-10-02 19:43:36 +0100

    Add syntax highlighting to the Note component.

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,17 +1,56 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-export default function({ content, timestamp }: { content: string, timestamp: number }) {
+interface NoteProps {
+  content: string;
+  timestamp: number;
+}
+
+export default function Note({ content, timestamp }: NoteProps) {
   const date = new Date(timestamp);
+
   return (
     <article className="note">
-      <ReactMarkdown>{content}</ReactMarkdown>
-      {date &&
+      <ReactMarkdown
+        components={{
+          code: CodeBlock
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+      {date && (
         <footer>
           <time dateTime={date.toISOString()}>
             {date.toLocaleString()}
           </time>
         </footer>
-      }
+      )}
     </article>
+  );
+}
+
+type CodeBlockProps = {
+  inline?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+};
+
+function CodeBlock({ inline, className, children, ...props }: CodeBlockProps) {
+  const match = /language-(\w+)/.exec(className || '');
+  return !inline && match ? (
+    <SyntaxHighlighter
+      style={vscDarkPlus}
+      language={match[1]}
+      PreTag="div"
+      {...props}
+    >
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
   );
 };
