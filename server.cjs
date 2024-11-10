acfbd7461a5004848f106b930453937e96d3c09b
Adam Kelly <adam@cthulahoops.org>
2024-11-10 12:46:24 +0000

    Add cache headers so the app will work offline.

--- server.cjs
+++ server.cjs
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'dist/assets' directory
-app.use("/assets", express.static(path.join(__dirname, "dist", "assets")));
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "dist", "assets"), {
+    maxAge: "1y",
+    immutable: true,
+  }),
+);
 
-// Serve index.html for all other routes
 app.get("*", (req, res) => {
+  res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
